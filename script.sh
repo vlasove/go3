85b93d2101dd3c5089aeacf31571616dfaf97aeb
vlasove <evgeny_vlasov@yahoo.com>
2020-02-10 09:39:09 +0300

    added .sh for docker + git file

---
+++ script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+sudo apt-get update
+#git
+sudo apt install git
+#vscode
+sudo snap install --classic code
+#docker 
+sudo apt-get update
+
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+#Docker verificator
+sudo docker run hello-world
