03e339a95246bf89896fa7f804675af0a69c3694
vlasove <evgeny_vlasov@yahoo.com>
2020-02-10 12:17:23 +0300

    Eq1

--- test/main.go
+++ test/main.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world!")
+	var a, b, c int
+	fmt.Scan(&a, &b, &c)
+
+	if a == 0 {
+		if b == 0 {
+			fmt.Println("Has No roots")
+		} else {
+			fmt.Println("Has 1 root")
+		}
+	} else {
+		D := b*b - 4*a*c
+		if D == 0 {
+			fmt.Println("Has 1 root")
+		} else if D > 0 {
+			fmt.Println("Has 2 roots")
+		} else {
+			fmt.Println("Has No roots")
+		}
+	}
 }
