cfc0e38e6cd84e13d4799372b4679ef62d55356d
vlasove <evgeny_vlasov@yahoo.com>
2020-02-10 15:33:16 +0300

    add proto.sh

---
+++ proto/proto.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get install autoconf automake libtool curl make g++ unzip -y
+git clone https://github.com/google/protobuf.git
+cd protobuf
+git submodule update --init --recursive
+./autogen.sh
+./configure
+make
+make check
+sudo make install
+sudo ldconfig
