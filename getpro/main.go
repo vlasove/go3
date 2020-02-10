a8d6b4c4995aa78a77a3a6cf3879533235d95936
vlasove <evgeny_vlasov@yahoo.com>
2020-02-10 14:42:13 +0300

    add http roots

--- getpro/main.go
+++ getpro/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
@@ -9,11 +10,13 @@ import (
 	"github.com/gorilla/mux"
 )
 
-//Person ...
-type Person struct {
-	Name     string `json:"name"`
-	Lastname string `json:"lastname"`
-	Age      int64  `json:"age"`
+// Equation ...
+type Equation struct {
+	A             int64  `json:"a"`
+	B             int64  `json:"b"`
+	C             int64  `json:"c"`
+	TotalEquation string `json:"total_equation"`
+	Result        string `json:"result"`
 }
 
 // getInfo ...
@@ -21,20 +24,57 @@ func getInfo(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 
-	p := Person{
-		Name:     params["name"],
-		Lastname: params["lastname"],
+	a, _ := strconv.ParseInt(params["a"], 10, 64)
+	b, _ := strconv.ParseInt(params["b"], 10, 64)
+	c, _ := strconv.ParseInt(params["c"], 10, 64)
+
+	answerEquation := fmt.Sprintf("%dx^2 + %dx + %d = 0", a, b, c)
+	result := Solve(int(a), int(b), int(c))
+
+	e := Equation{
+		A:             a,
+		B:             b,
+		C:             c,
+		TotalEquation: answerEquation,
+		Result:        result,
 	}
-	age, err := strconv.ParseInt(params["age"], 10, 64)
-	if err != nil {
-		log.Fatal(err)
-	}
-	p.Age = age
-	json.NewEncoder(w).Encode(p)
+	json.NewEncoder(w).Encode(e)
+
 }
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/{name}/{lastname}/{age}", getInfo).Methods("GET")
+	r.HandleFunc("/{a}/{b}/{c}", getInfo).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+// Solve ...
+func Solve(a, b, c int) string {
+	var result string
+	if a == 0 {
+		result = Linear(b, c)
+	} else {
+		result = Quadratic(a, b, c)
+	}
+	return result
+}
+
+//Linear ...
+func Linear(b, c int) string {
+	if b == 0 {
+		return "Has No roots"
+	}
+	return "Has 1 root"
+}
+
+// Quadratic ...
+func Quadratic(a, b, c int) string {
+	D := b*b - 4*a*c
+	if D == 0 {
+		return "Has 1 root"
+	} else if D > 0 {
+		return "Has 2 roots"
+	}
+	return "Has No roots"
+
 }
