50438c81370146363da2b912c18acb3476e3c6df
vlasove <evgeny_vlasov@yahoo.com>
2020-02-11 16:21:51 +0300

    micro go

--- tasks_go3/task2/main.go
+++ tasks_go3/task2/main.go
@@ -1,9 +1,20 @@
 package main
 
-func Pyramid(n int) string {
+import (
+	"fmt"
+	"strings"
+)
 
+func Pyramid(n int) string {
+	var result string
+	for i := 0; i < n; i++ {
+		result += strings.Repeat(" ", n-(i+1))
+		result += strings.Repeat("@", (i*2)+1)
+		result += "\n"
+	}
+	return strings.TrimRight(result, "\n")
 }
 
 func main() {
-
+	fmt.Print(Pyramid(2))
 }
