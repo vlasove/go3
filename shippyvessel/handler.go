20cb6ffb6032f3ed9775d05cbbd00a8baaeba721
vlasove <evgeny_vlasov@yahoo.com>
2020-02-13 12:45:56 +0300

    wo examples

--- shippyvessel/handler.go
+++ shippyvessel/handler.go
@@ -16,14 +16,5 @@ func (s *service) GetRepo() Repository {
 }
 
 func (s *service) FindAvailable(ctx context.Context, req *pb.Specification, res *pb.Response) error {
-	defer s.GetRepo().Close()
-	// Find the next available vessel
-	vessel, err := s.GetRepo().FindAvailable(req)
-	if err != nil {
-		return err
-	}
 
-	// Set the vessel as part of the response message type
-	res.Vessel = vessel
-	return nil
 }
