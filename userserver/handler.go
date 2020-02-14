9ebd5a9a3ccc35c5c71926589be522f33e317139
vlasove <evgeny_vlasov@yahoo.com>
2020-02-14 11:48:56 +0300

    need to update jwt

--- userserver/handler.go
+++ userserver/handler.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"errors"
+	"log"
 
 	pb "github.com/vlasove/userserver/proto/user"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type service struct {
@@ -12,6 +15,12 @@ type service struct {
 }
 
 func (srv *service) Create(ctx context.Context, req *pb.User, res *pb.Response) error {
+
+	hashedPass, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	req.Password = string(hashedPass)
 	if err := srv.repo.Create(req); err != nil {
 		return err
 	}
@@ -36,15 +45,35 @@ func (srv *service) GetAll(ctx context.Context, req *pb.Request, res *pb.Respons
 }
 
 func (srv *service) Auth(ctx context.Context, req *pb.User, res *pb.Token) error {
-	_, err := srv.repo.GetByEmailAndPassword(req)
+	log.Println("Logging with:", req.Email, req.Password)
+
+	user, err := srv.repo.GetByEmailAndPassword(req)
 	if err != nil {
 		return err
 	}
-	res.Token = "myTestToken123"
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(req.Password)); err != nil {
+		return err
+	}
+	token, err := srv.tokenService.Encode(user)
+	if err != nil {
+		return err
+	}
+	res.Token = token
 	return nil
 }
 
 //ValidateToken ...
 func (srv *service) ValidateToken(ctx context.Context, req *pb.Token, res *pb.Token) error {
+	cliam, err := srv.tokenService.Decode(req.Token)
+	if err != nil {
+		return err
+	}
+	if cliam.User.Id == "" {
+		return errors.New("invalid user")
+	}
+
+	res.Valid = true
+
 	return nil
 }
--- userserver/token_service.go
+++ userserver/token_service.go
@@ -1,8 +1,20 @@
 package main
 
+import (
+	"github.com/dgrijalva/jwt-go"
+	pb "github.com/vlasove/userserver/proto/user"
+)
+
+var key = []byte("MyNewSecretKey99999")
+
+type MyClaim struct {
+	User *pb.User
+	jwt.StandardClaims
+}
+
 type Authable interface {
-	Encode(data interface{}) (string, error)
-	Decode(token string) (interface{}, error)
+	Encode(user *pb.User) (string, error)
+	Decode(token string) (*MyClaim, error)
 }
 
 type TokenService struct {
@@ -10,9 +22,28 @@ type TokenService struct {
 }
 
 func (ts *TokenService) Decode(token string) (interface{}, error) {
-	return "", nil
+	tokenType, err := jwt.ParseWithClaims(string(key), &MyClaim{}, func(token *jwt.Token) (interface{}, error) {
+		return key, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if claim, isOk := tokenType.Claims.(*MyClaim); isOk && tokenType.Valid {
+		return claim, nil
+	}
+	return nil, err
+
 }
 
-func (ts *TokenService) Encode(data interface{}) (string, error) {
-	return "", nil
+func (ts *TokenService) Encode(user *pb.User) (string, error) {
+	claim := MyClaim{
+		user,
+		jwt.StandardClaims{
+			ExpiresAt: 20000,
+			Issuer:    "user",
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
+	return token.SignedString(key)
 }
